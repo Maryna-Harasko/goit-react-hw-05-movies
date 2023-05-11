efdcec436470fdc7b4d4936f37e5558a0d63d426
Maryna-Harasko <mngarasko@gmail.com>
2023-05-11 17:49:33 +0300

    color loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ import { LoaderContainer } from "./Loader.styled";
 export const Loader = () => {
   return(
     <LoaderContainer>
-      <ThreeDots color="#3f51b5"/>
+      <ThreeDots color="#212b62"/>
     </LoaderContainer>
   )
 }
